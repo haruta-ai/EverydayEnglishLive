e98a08b3fc76b3d3c8423cd79b2c81296214c752
haruta-ai <y.haruta.0830@gmail.com>
2026-07-14 22:51:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-const CACHE = "eel-v0.4";
-const FILES = [
-  "./","./index.html","./style.css?v=040","./script.js?v=040",
-  "./manifest.webmanifest","./icon-192.png","./icon-512.png"
-];
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+const C="eel-v0.5";const F=["./","./index.html","./style.css?v=050","./script.js?v=050","./manifest.webmanifest","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
